e5cad59675a88a65dd3d7a41b4f79e0f69c382e6
soplinger <solekibbles4@gmail.com>
2018-09-13 19:23:55 -0400

    i wish i got to pet a dolphin but my mom took me out of daycare early

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,25 @@
 package com.company;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        System.out.println("Enter an integer: ");
+
+        Scanner integerInput = new Scanner(System.in);
+
+        int intInput = integerInput.nextInt();
+
+        if (intInput%2 == 0)
+        {
+            System.out.println("The Integer " + intInput + " is even");
+        }
+        else {
+            System.out.println("The Integer " + intInput + " is odd");
+        }
+
+
+
+
     }
 }
